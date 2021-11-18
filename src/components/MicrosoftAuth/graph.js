d8b190af33b3013add484380bfdea0fb6e1033e9
EnumC <eric@enumc.com>
2021-11-17 23:15:26 -0800

    add: error handling for XHR client-side error

--- src/components/MicrosoftAuth/graph.js
+++ src/components/MicrosoftAuth/graph.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 import { getCookie } from '../CookieHelper';
 export const getProfileBase64 = async (token) => {
     const graphEndpoint = "https://graph.microsoft.com/v1.0/me/photo/$value";
-
-    const response = await axios(graphEndpoint, { headers: { Authorization: `Bearer ${token}` }, responseType: 'arraybuffer' });
-    const avatar = Buffer.from(response.data, 'binary').toString('base64');
-    // console.log("avatar: ", avatar);
-    return avatar;
+    try {
+        const response = await axios(graphEndpoint, { headers: { Authorization: `Bearer ${token}` }, responseType: 'arraybuffer' });
+        const avatar = Buffer.from(response.data, 'binary').toString('base64');
+        // console.log("avatar: ", avatar);
+        return avatar;
+    }
+    catch (error) {
+        console.error(Object.keys(error), error.message); 
+    }
+    
 }
 export const getAccessToken = () => {
     // return a promise with accessToken string
@@ -18,9 +23,14 @@ export const getAccessToken = () => {
   export const updateResume = async (token, fileContent) => {
     // const graphEndpoint = "https://graph.microsoft.com/v1.0/me/drive/root:/HKN_Persistant_Storage/resume.pdf:/content";
     const graphEndpoint = "https://graph.microsoft.com/v1.0/me/drive/special/approot:/resume.pdf:/content";
-    const response = await axios.put(graphEndpoint, fileContent, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
-    console.log("response: ", response);
-    return response;
+    try {
+        const response = await axios.put(graphEndpoint, fileContent, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
+        console.log("response: ", response);
+        return response;
+    }
+    catch (error) {
+        return { status: 408, statusText: error.message, data: "" };
+    }
     // const avatar = Buffer.from(response.data, 'binary').toString('base64');
     // console.log("avatar: ", avatar);
     // return avatar;
@@ -29,15 +39,25 @@ export const getAccessToken = () => {
   export const getSharingLink = async (token, fileID) => {
     const graphEndpoint = "https://graph.microsoft.com/v1.0/me/drive/items/" + fileID + "/createLink";
     const payload = { type: 'view', scope: 'organization' }; // embed only supports anonymous scope.
-    const response = await axios.post(graphEndpoint, payload, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
-    console.log("response: ", response);
-    return response;
+    try {
+        const response = await axios.post(graphEndpoint, payload, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
+        console.log("response: ", response);
+        return response;
+    }
+    catch (error) {
+        return { status: 408, statusText: error.message, data: "" };
+    }
   }
 
   export const getPreviewEmbed = async (token, fileID) => {
     const graphEndpoint = "https://graph.microsoft.com/v1.0/me/drive/items/" + fileID + "/preview";
-    const payload = {  }; // embed only supports anonymous scope.
-    const response = await axios.post(graphEndpoint, payload, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
-    console.log("response: ", response);
-    return response;
+    const payload = {  };
+    try {
+        const response = await axios.post(graphEndpoint, payload, { headers: { Authorization: `Bearer ${token}` }, responseType: 'json' });
+        console.log("response: ", response);
+        return response;
+    }
+    catch (error) {
+        return { status: 408, statusText: error.message, data: "" };
+    }
   }
